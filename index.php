967a9edac76bfb05a8e889e2daf670111d22ec08
Martin <support@coderspress.com>
2015-07-29 08:28:52 +0100

    Wordpress 4.2.3

--- index.php
+++ index.php
@@ -3,12 +3,12 @@
 Plugin Name: Title On Invoice
 Plugin URI: http://coderspress.com/
 Description: Adds Listing Titles to Invoices and payments
-Version: 2015.0605
-Updated: 5th June 2015
+Version: 2015.0729
+Updated: 29th July 2015
 Author: sMarty 
 Author URI: http://coderspress.com
 WP_Requires: 3.8.1
-WP_Compatible: 4.2.2
+WP_Compatible: 4.2.3
 License: http://creativecommons.org/licenses/GPL/2.0
 */
 
